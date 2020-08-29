9a439166be549afe9a5cced1a2df5a2e76281a98
AppVeyor <ci@appveyor.com>
2020-08-29 14:50:01 +0000

    Updates

---
+++ precache-manifest.4e6ec01aa76901ab5374d6bb36af5335.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf5e09ac71742a07b368a3808baeea9",
+    "url": "/bosa-noga-frontend/index.html"
+  },
+  {
+    "revision": "789133848163a98f4790",
+    "url": "/bosa-noga-frontend/static/css/main.9409142a.chunk.css"
+  },
+  {
+    "revision": "ffcc8f50f5ec2326b445",
+    "url": "/bosa-noga-frontend/static/js/2.2ffee260.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bosa-noga-frontend/static/js/2.2ffee260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789133848163a98f4790",
+    "url": "/bosa-noga-frontend/static/js/main.97446654.chunk.js"
+  },
+  {
+    "revision": "a4e63d8252780815a62c",
+    "url": "/bosa-noga-frontend/static/js/runtime-main.57202793.js"
+  },
+  {
+    "revision": "da7154cc0ce9cb91c3537f97953d132f",
+    "url": "/bosa-noga-frontend/static/media/bg.da7154cc.png"
+  },
+  {
+    "revision": "3ad87d7b75f31c11e5feb99a0da0c1a0",
+    "url": "/bosa-noga-frontend/static/media/cat.3ad87d7b.png"
+  }
+]);
